9dc919b97e52de0148d7615cb39bac004a619c7d
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2021-04-25 06:12:06 +0000

    more common types
    using pub(crate) to be able to create some objects for testing

--- src/v2/json_api/common_structs.rs
+++ src/v2/json_api/common_structs.rs
@@ -1,5 +1,7 @@
 //! Common structs used by multiple B2 API calls
 
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
@@ -26,8 +28,68 @@ pub struct MachineReadableJsonErrorObj<'a> {
     pub code: &'a str,
 }
 
+/// Invalid data was received, the contents of message are subject to change,
+/// so please do not implement logic based on those.
+#[derive(Debug)]
+pub struct InvalidData {
+    message: String,
+}
+
+impl InvalidData {
+    pub fn new(message: String) -> Self {
+        Self { message }
+    }
+
+    /// Get a reference to the invalid data's message.
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+}
+
+impl Display for InvalidData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Invalid Data Received: {}", self.message)
+    }
+}
+
+impl std::error::Error for InvalidData {}
+
 #[derive(Debug)]
 pub enum Error {
     JSONError(JsonErrorObj),
     ReqwestError(reqwest::Error),
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct ApiUrl(pub(crate) String);
+
+impl ApiUrl {
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct DownloadUrl(String);
+
+impl DownloadUrl {
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct AccountId(pub(crate) String);
+
+impl AccountId {
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct AuthorizationToken(pub(crate) String);
+
+impl AuthorizationToken {
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
 }

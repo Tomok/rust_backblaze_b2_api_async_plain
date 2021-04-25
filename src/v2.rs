911725a478a809b1cf704225342ccd71dd498e19
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2021-04-25 07:01:38 +0000

    create public version of b2_authorize_account

--- src/v2.rs
+++ src/v2.rs
@@ -5,7 +5,21 @@ mod b2_authorize_account;
 mod b2_list_buckets;
 mod buckets;
 
-pub use b2_authorize_account::b2_authorize_account;
+pub use b2_authorize_account::{AuthorizeAccountOk, AuthorizeError};
+/// Authorize account function see [official documentation](https://www.backblaze.com/b2/docs/b2_authorize_account.html)
+pub async fn b2_authorize_account(
+    application_key_id: &str,
+    application_key: &str,
+) -> Result<AuthorizeAccountOk, AuthorizeError> {
+    // call the real function with the basic_uri filled in (needes to be changeable for testing)
+    b2_authorize_account::b2_authorize_account(
+        "https://api.backblazeb2.com",
+        application_key_id,
+        application_key,
+    )
+    .await
+}
+
 pub use b2_list_buckets::b2_list_buckets;
 pub use common_structs::*;
 

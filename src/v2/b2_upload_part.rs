b0aa1318c30bd360388bb84ee9dc6cc0d1b71675
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2021-08-19 18:14:02 +0000

    some documentation for b2_upload_part

--- src/v2/b2_upload_part.rs
+++ src/v2/b2_upload_part.rs
@@ -13,24 +13,34 @@ use super::{
 #[derive(Debug, Serialize, TypedBuilder)]
 pub struct UploadPartParameters {
     #[serde(rename = "X-Bz-Part-Number")]
+    /// A number from 1 to 10000. The parts uploaded for one file must have contiguous numbers, starting with 1.
     part_number: PartNumber,
 
     #[serde(rename = "Content-Length")]
+    /// The number of bytes in the file being uploaded. Note that this header is required; you cannot leave it out and just use chunked encoding.
+    /// The minimum size of every part but the last one is 5MB.
+    /// When sending the SHA1 checksum at the end, the Content-Length should be set to the size of the file plus the 40 bytes of hex checksum.
     content_length: u64,
 
     #[serde(rename = "X-Bz-Content-Sha1")]
+    /// The SHA1 checksum of the this part of the file. B2 will check this when the part is uploaded, to make sure that the data arrived correctly.
+    /// The same SHA1 checksum must be passed to b2_finish_large_file.
+    /// You may optionally provide the SHA1 at the end of the upload.
     content_sha1: Sha1,
 
     #[serde(rename = "X-Bz-Server-Side-Encryption-Customer-Algorithm")]
     #[builder(default, setter(strip_option))]
+    /// This header is required if b2_start_large_file was called with parameters specifying Server-Side Encryption with Customer-Managed Keys (SSE-C), in which case its value must match the serverSideEncryption algorithm requested via [b2_start_large_file].
     server_side_encryption_algorithm: Option<EncryptionAlgorithm>,
 
     #[serde(rename = "X-Bz-Server-Side-Encryption-Customer-Key")]
     #[builder(default, setter(strip_option))]
+    /// This header is required if b2_start_large_file was called with parameters specifying Server-Side Encryption with Customer-Managed Keys (SSE-C), in which case its value must match the serverSideEncryption customerKey requested via [b2_start_large_file].
     server_side_encryption_customer_key: Option<ServerSideEncryptionCustomerKey>,
 
     #[serde(rename = "X-Bz-Server-Side-Encryption-Customer-Key-Md5")]
     #[builder(default, setter(strip_option))]
+    /// This header is required if b2_start_large_file was called with parameters specifying Server-Side Encryption with Customer-Managed Keys (SSE-C), in which case its value must match the serverSideEncryption customerKeyMd5 requested via [b2_start_large_file].
     server_side_encryption_customer_key_md5: Option<Md5>,
 }
 

7d941055eb573a17578454619ae796ce4c5f4d48
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2021-08-28 19:50:03 +0200

    Use references

--- src/v2/b2_delete_file_version.rs
+++ src/v2/b2_delete_file_version.rs
@@ -7,15 +7,29 @@ use super::{
 
 #[derive(Debug, Serialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
-pub struct DeleteFileVersionRequest {
-    file_name: FileName,
+pub struct DeleteFileVersionRequest<'s> {
+    file_name: &'s FileName,
     /// The ID of the file, as returned by b2_upload_file, b2_list_file_names, or b2_list_file_versions.
-    file_id: FileId,
+    file_id: &'s FileId,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// Must be specified and set to true if deleting a file version protected by File Lock governance mode retention settings. Setting the value requires the bypassGovernance application key capability.
     bypass_governance: Option<bool>,
+}
+
+impl<'s> DeleteFileVersionRequest<'s> {
+    pub fn new(
+        file_name: &'s FileName,
+        file_id: &'s FileId,
+        bypass_governance: Option<bool>,
+    ) -> Self {
+        Self {
+            file_name,
+            file_id,
+            bypass_governance,
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -30,10 +44,10 @@ pub struct DeleteFileVersionOk {
 /// If the version you delete is the latest version, and there are older versions, then the most recent older version will become the current version, and be the one that you'll get when downloading by name. See the File Versions page for more details.
 ///
 ///When used on an unfinished large file, this call has the same effect as b2_cancel_large_file.
-pub async fn b2_delete_file_version(
-    api_url: &ApiUrl,
-    authorization_token: &AuthorizationToken,
-    request: &DeleteFileVersionRequest,
+pub async fn b2_delete_file_version<'a>(
+    api_url: &'a ApiUrl,
+    authorization_token: &'a AuthorizationToken,
+    request: &'a DeleteFileVersionRequest<'a>,
 ) -> Result<DeleteFileVersionOk, DeleteFileVersionError> {
     let url = format!("{}/b2api/v2/b2_delete_file_version", api_url.as_str());
     let resp = reqwest::Client::new()

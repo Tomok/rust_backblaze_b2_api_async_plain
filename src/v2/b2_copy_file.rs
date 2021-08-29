e40e925f236ce7bafd7b6cb04aab6e6b55c1eacf
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2021-08-29 17:52:45 +0000

    use references

--- src/v2/b2_copy_file.rs
+++ src/v2/b2_copy_file.rs
@@ -36,23 +36,23 @@ pub enum MetadataDirective {
 
 #[derive(Debug, Serialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
-pub struct CopyFileRequest {
+pub struct CopyFileRequest<'s> {
     ///The ID of the source file being copied.
-    source_file_id: FileId,
+    source_file_id: &'s FileId,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// The ID of the bucket where the copied file will be stored. If this is not set, the copied file will be added to the same bucket as the source file.
     /// Note that the bucket containing the source file and the destination bucket must belong to the same account.
-    destination_bucket_id: Option<BucketId>,
+    destination_bucket_id: Option<&'s BucketId>,
 
     /// The name of the new file being created.
-    file_name: FileName,
+    file_name: &'s FileName,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// The range of bytes to copy. If not provided, the whole source file will be copied.
-    range: Option<Range>,
+    range: Option<&'s Range>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -63,40 +63,40 @@ pub struct CopyFileRequest {
     #[serde(skip_serializing_if = "Option::is_none")]
     /// Must only be supplied if the metadataDirective is REPLACE.
     /// The MIME type of the content of the file, which will be returned in the Content-Type header when downloading the file.
-    content_type: Option<Mime>,
+    content_type: Option<&'s Mime>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// Must only be supplied if the metadataDirective is REPLACE.
     /// This field stores the metadata that will be stored with the file.
     /// It follows the same rules that are applied to b2_upload_file
-    file_info: Option<FileInfo>,
+    file_info: Option<&'s FileInfo>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// If present, specifies the File Lock retention settings for the new file. Setting the value requires the writeFileRetentions capability and that the destination bucket is File Lock-enabled.
-    file_retention: Option<FileRetention>,
+    file_retention: Option<&'s FileRetention>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// If present, specifies the File Lock legal hold status for the new file. Setting the value requires the writeFileLegalHolds capability and that the destination bucket is File Lock-enabled.
-    legal_hold: Option<LegalHold>,
+    legal_hold: Option<&'s LegalHold>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// If present, specifies the parameters for Backblaze B2 to use for accessing the source file data using Server-Side Encryption. This parameter is required if and only if the source file has been encrypted using Server-Side Encryption with Customer-Managed Keys (SSE-C), and the provided encryption key must match the one with which the source file was encrypted.
-    source_server_side_encryption: Option<ServerSideEncryptionCustomerKey>,
+    source_server_side_encryption: Option<&'s ServerSideEncryptionCustomerKey>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// If present, specifies the parameters for Backblaze B2 to use for encrypting the copied data before storing the destination file using Server-Side Encryption.
-    destination_server_side_encryption: Option<ServerSideEncryptionCustomerKey>,
+    destination_server_side_encryption: Option<&'s ServerSideEncryptionCustomerKey>,
 }
 
-pub async fn b2_copy_file(
+pub async fn b2_copy_file<'a>(
     api_url: &ApiUrl,
     authorization_token: &AuthorizationToken,
-    request: &CopyFileRequest,
+    request: &CopyFileRequest<'a>,
 ) -> Result<FileInformation, errors::CopyError> {
     let url = format!("{}/b2api/v2/b2_copy_file", api_url.as_str());
     let request = reqwest::Client::new()

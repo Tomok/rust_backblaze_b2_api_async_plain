4eb3a0837c8d7c27f554d44dd366575183a77df7
Thomas Lahmer <791550+Tomok@users.noreply.github.com>
2021-09-10 19:22:34 +0200

    Use references

--- src/v2/b2_update_bucket.rs
+++ src/v2/b2_update_bucket.rs
@@ -11,52 +11,52 @@ use super::{
 
 #[derive(Debug, Serialize, TypedBuilder)]
 #[serde(rename_all = "camelCase")]
-pub struct UpdateBucketRequest {
+pub struct UpdateBucketRequest<'s> {
     /// Your account ID.
-    account_id: AccountId,
+    account_id: &'s AccountId,
     /// The name to give the new bucket.
-    bucket_id: BucketId,
+    bucket_id: &'s BucketId,
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
-    bucket_type: Option<BucketType>, // TODO: use differnt type, that allows all private / all public only
+    bucket_type: Option<&'s BucketType>, // TODO: use differnt type, that allows all private / all public only
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// User-defined information to be stored with the bucket: a JSON object mapping names to values. See Buckets.
     ///Cache-Control policies can be set here on a global level for all the files in the bucket.
-    bucket_info: Option<BucketInfo>,
+    bucket_info: Option<&'s BucketInfo>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
-    cors_rules: Option<serde_json::Value>, //TODO...
+    cors_rules: Option<&'s serde_json::Value>, //TODO...
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// The default File Lock retention settings for this bucket.
     ///
     ///If specified, the existing default bucket retention settings will be replaced with the new settings. If not specified, setting will remain unchanged. Setting the value requires the writeBucketRetentions capability and that the bucket is File Lock-enabled.  
-    default_retention: Option<DefaultFileRetention>,
+    default_retention: Option<&'s DefaultFileRetention>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// The default server-side encryption settings for this bucket.
-    default_server_side_encryption: Option<ServerSideEncryption>,
+    default_server_side_encryption: Option<&'s ServerSideEncryption>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// The initial list of lifecycle rules for this bucket.
-    lifecycle_rules: Option<Vec<LifeCycleRule>>,
+    lifecycle_rules: Option<&'s Vec<LifeCycleRule>>,
 
     #[builder(default, setter(strip_option))]
     #[serde(skip_serializing_if = "Option::is_none")]
     /// When set, the update will only happen if the revision number stored in the B2 service matches the one passed in. This can be used to avoid having simultaneous updates make conflicting changes.
-    if_revision_is: Option<BucketRevision>,
+    if_revision_is: Option<&'s BucketRevision>,
 }
 
-pub async fn b2_update_bucket(
+pub async fn b2_update_bucket<'a>(
     api_url: &ApiUrl,
     authorization_token: &AuthorizationToken,
-    request: &UpdateBucketRequest,
+    request: &UpdateBucketRequest<'a>,
 ) -> Result<Bucket, UpdateBucketError> {
     let url = format!("{}/b2api/v2/b2_update_bucket", api_url.as_str());
     let request = reqwest::Client::new()
